0b2cb8bbf5397088765468bbf7e7deab366c6fc1
Fernando Cortés <fernando@cortescalderon.com>
2026-03-07 21:14:07 -0600

    Initial commit: add HolaMundo, presentacion and gitignore

---
+++ src/Presentacion.java
@@ -0,0 +1,7 @@
+public class Presentacion {
+    static void main() {
+        System.out.println("Fernando Cortés Calderón");
+        System.out.println("Edad: 33 años");
+        System.out.println("País: México");
+    }
+}
